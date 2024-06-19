45706eb9eb2617e7bb42965607fa577c30ad155f
Rahul Lokhande <rlokhande06244@gmail.com>
2024-06-19 16:00:37 +0530

    Python_Indexing

---
+++ indexing.py
@@ -0,0 +1,10 @@
+my_list=[1,2,3,4,"Rahul",5]
+my_tuple=(6,7,8,9,10)
+print(my_list.index(4))
+# print(my_tuple.index(4))
+print(my_list.index("Rahul"))
+# name="RAHUL"
+# print(name.index("R"))
+# print(name.index("H",2))
+# print(name.index("H",2,5))
+# # print(name.index("H",(name.index("H")+1)))
