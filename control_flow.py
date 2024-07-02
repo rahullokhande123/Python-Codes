d521051f3fbbe561685c76acf8704556e8f8dc82
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-02 16:09:30 +0530

    Condition,Loop

--- control_flow.py
+++ control_flow.py
@@ -1,7 +1,47 @@
-age=int(input("Enter Your Age :"))
-if age>18:
-    print("YOU ARE VALIDE FOR VOTING")
-else:
-    print("You are not eligible for votting")
+# age=int(input("Enter Your Age :"))
+# if age>18:
+#     print("YOU ARE VALIDE FOR VOTING")
+# else:
+#     print("You are not eligible for votting")
     
-print("Thank You For Visiting")
+# print("Thank You For Visiting")
+
+
+a=range(10)
+print(a)
+print(list(a))
+print(tuple(a))
+print(set(a))
+# Even No. in - way
+b=range(-2,-11,-2)
+# odd No. in -way
+c=range(-1,-11,-2)
+# Positive odd No.
+d=range(2,11,2)
+print(list(b))
+print(list(c))
+print(list(d))
+# =========================================
+
+my_list=[]
+for i in range(2,11,2):
+    x=i**2
+    my_list.append(x)
+print(my_list)
+
+# ========================================
+
+my_str=input("Enter Your Name : ")
+total_char=0
+Vowel=0
+Consonent=0
+for i in my_str:
+    total_char=total_char+1
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
+        Vowel=Vowel+1
+    else:
+        Consonent=Consonent+1
+    
+print(total_char)
+print(Vowel)
+print(Consonent)
