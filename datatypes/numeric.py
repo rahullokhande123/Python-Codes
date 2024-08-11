37bfd722210b3f9f63db6bde9136626244b41944
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-11 15:34:54 +0530

    Datatypes

--- datatypes/numeric.py
+++ datatypes/numeric.py
@@ -1,10 +1,12 @@
+# ======================= Arithmetic Oprater ====================== 
+
 x=20
 y=10
 z1=x+y
 z2=x*y
 z3=x/y
 print(z1)
-print(type(z1))  #type ka use hm ->  variable ka data show krne ke liye  
+print(type(z1))  #type ka use hm ->  variable ka data type show krne ke liye
 print(id(z1))
 
 print(type(z2))
@@ -13,7 +15,7 @@ print(id(z2))     #id ka use hm ->  variable ka address show krne ke liye
 print(type(z3))
 print(id(z3))
 
-#============================= Float ============================
+#========================== Float ============================
 
 x=20.0
 y=10.0
